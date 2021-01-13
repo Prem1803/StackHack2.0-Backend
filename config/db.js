934aaaeb0b2d5e46ee065d21fabcf71feda606ee
DIvyanshu Bose <56763659+theventurer@users.noreply.github.com>
2021-01-13 16:46:56 +0530

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -1,21 +1,15 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
+        console.log(process.env.MONGO_URL)
+    const connection = await mongoose.connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
-        useUnifiedTopology: true,
-        });
-        console.log("MongoDB connected");
-    } 
-    catch (err) {
-      console.log(err.message);
-      process.exit(1);
-    }
-};
+        useUnifiedTopology: true
+    });
+
+    console.log(`MongoDB Connected: ${connection.connection.host}`);
+}; 
 
 module.exports = connectDB;
